a4c8f6450db115ab6f516f4151f732f75ea636f9
Augusto Bardini <azbardini@gmail.com>
2019-10-23 03:20:11 -0300

    :fire: Removing some code

--- src/textplayer/Manager.java
+++ src/textplayer/Manager.java
@@ -9,7 +9,6 @@ import java.io.File;
 import org.jfugue.midi.MidiFileManager;
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
-import org.jfugue.theory.ChordProgression;
 
 /**
  *
